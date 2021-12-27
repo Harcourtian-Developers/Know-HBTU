ab066a2f3a6b20f1cf1fcd6ef03f88e12b069eab
Raunak173 <raunakagarwal173@gmail.com>
2021-12-27 16:35:15 +0530

    header completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
+import Header from "./components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Resources from "./components/Resources";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div>
-      <h1>APP</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/resources" element={<Resources />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/Header.js
@@ -0,0 +1,247 @@
+import React, { useState, useEffect } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  makeStyles,
+  Button,
+  Drawer,
+  List,
+  useTheme,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+import logo from "../images/logo.jpeg";
+import MenuIcon from "@material-ui/icons/Menu";
+import IconButton from "@material-ui/core/IconButton";
+import clsx from "clsx";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+const drawerWidth = 150;
+
+const useStyles = makeStyles((theme) => ({
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    height: "15vh",
+    [theme.breakpoints.down("md")]: {
+      width: "100vw",
+      height: "10vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+    },
+  },
+  image: {
+    height: "100px",
+    [theme.breakpoints.down("md")]: {
+      height: "70px",
+    },
+  },
+  icon: {
+    fontSize: "100px",
+    [theme.breakpoints.down("md")]: {
+      fontSize: "50px",
+    },
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  const [menu, setMenu] = useState(window.innerWidth > 1000);
+  const [open, setOpen] = useState(false);
+  const theme = useTheme();
+  const updateMedia = () => {
+    setMenu(window.innerWidth > 1000);
+  };
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
+  return (
+    <>
+      <AppBar
+        style={{
+          backgroundColor: "white",
+          color: "black",
+        }}
+      >
+        <Toolbar className={classes.toolbar}>
+          <img src={logo} className={classes.image} />
+          {menu && (
+            <>
+              <Typography>
+                <Link
+                  to="/"
+                  style={{
+                    color: "inherit",
+                    textDecoration: "none",
+                    marginLeft: "750px",
+                  }}
+                >
+                  Home
+                </Link>
+              </Typography>
+              <Typography>
+                <Link
+                  to="/resources"
+                  style={{
+                    color: "inherit",
+                    textDecoration: "none",
+                    marginLeft: "30px",
+                  }}
+                >
+                  Resources
+                </Link>
+              </Typography>
+              <Typography>
+                <Link
+                  to="/about"
+                  style={{
+                    color: "inherit",
+                    textDecoration: "none",
+                    marginLeft: "30px",
+                  }}
+                >
+                  About
+                </Link>
+              </Typography>
+              <Button variant="contained" style={{ marginLeft: "30px" }}>
+                <a
+                  href="https://erp.hbtu.ac.in/"
+                  style={{
+                    color: "inherit",
+                    textDecoration: "none",
+                  }}
+                >
+                  HBTU ERP
+                </a>
+              </Button>
+              <Button variant="contained" style={{ marginLeft: "30px" }}>
+                <a
+                  href="https://hbtu.ac.in/"
+                  style={{
+                    color: "inherit",
+                    textDecoration: "none",
+                  }}
+                >
+                  Official Site
+                </a>
+              </Button>
+            </>
+          )}
+          {!menu && (
+            <>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="end"
+                onClick={handleDrawerOpen}
+                className={clsx(open && classes.hide)}
+              >
+                <MenuIcon />
+              </IconButton>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="right"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </div>
+        <List style={{ width: "100%" }}>
+          <Typography>
+            <Link
+              to="/"
+              style={{
+                color: "inherit",
+                textDecoration: "none",
+                paddingLeft: "40px",
+              }}
+            >
+              Home
+            </Link>
+          </Typography>
+          <Typography>
+            <Link
+              to="/resources"
+              style={{
+                color: "inherit",
+                textDecoration: "none",
+                paddingLeft: "30px",
+              }}
+            >
+              Resources
+            </Link>
+          </Typography>
+          <Typography>
+            <Link
+              to="/about"
+              style={{
+                color: "inherit",
+                textDecoration: "none",
+                paddingLeft: "40px",
+              }}
+            >
+              About
+            </Link>
+          </Typography>
+          <Button variant="contained" style={{ width: "100%" }}>
+            <a
+              href="https://erp.hbtu.ac.in/"
+              style={{
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              HBTU ERP
+            </a>
+          </Button>
+          <Button variant="contained" style={{ width: "100%" }}>
+            <a
+              href="https://hbtu.ac.in/"
+              style={{
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              Official Site
+            </a>
+          </Button>
+        </List>
+      </Drawer>
+    </>
+  );
+};
+
+export default Header;
